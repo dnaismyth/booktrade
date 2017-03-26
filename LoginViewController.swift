a8313c50b20b090e46f6394dbdbfc27e15a6591a
Dayna Naismyth <naismythdayna@gmail.com>
2017-03-25 19:25:38 -0700

    Setup storyboard layout

---
+++ LoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.swift
+//  booktrader
+//
+//  Created by Dayna Naismyth on 2017-03-25.
+//  Copyright © 2017 Dayna Naismyth. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Check that email input is provided
+    func validateEmail() -> String {
+        let email: String! = self.emailTextField.text
+        if(email.characters.count <= 0){
+            self.showInvalidAlert(alertTitle:"Error", alertMessage:"Please enter valid credentials.")
+        }
+        return email;
+    }
+    
+    // Check that password input is provided
+    func validatePassword() -> String {
+        let password: String! = self.passwordTextField.text
+        if(password.characters.count <= 0){
+            self.showInvalidAlert(alertTitle:"Error", alertMessage:"Please enter valid credentials.")
+        }
+        return password;
+    }
+    
+    private func showInvalidAlert(alertTitle: String, alertMessage: String){
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
